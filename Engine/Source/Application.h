322df68ea4ba963593818e7595ec5360101db828
carlosgspsp <masterupcgames2324@gmail.com>
2023-12-12 18:36:29 +0100

    First Commit

---
+++ Engine/Source/Application.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include<list>
+#include "Globals.h"
+#include "Module.h"
+
+class ModuleOpenGL;
+class ModuleWindow;
+class ModuleTextures;
+class ModuleInput;
+class ModuleRenderExercise;
+
+class Application
+{
+public:
+
+	Application();
+	~Application();
+
+	bool Init();
+	update_status Update();
+	bool CleanUp();
+
+    ModuleOpenGL* GetOpenGL() { return render; }
+    ModuleWindow* GetWindow() { return window; }
+    ModuleInput*  GetInput() { return input; }
+
+private:
+
+    ModuleOpenGL* render = nullptr;
+    ModuleWindow* window = nullptr;
+    ModuleInput* input = nullptr;
+
+    std::list<Module*> modules;
+
+};
+
+extern Application* App;
---
+++ Engine/Source/ModuleOpenGL.cpp
@@ -0,0 +1,54 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleOpenGL.h"
+#include "ModuleWindow.h"
+#include "SDL.h"
+
+ModuleOpenGL::ModuleOpenGL()
+{
+}
+
+// Destructor
+ModuleOpenGL::~ModuleOpenGL()
+{
+}
+
+// Called before render is available
+bool ModuleOpenGL::Init()
+{
+	LOG("Creating Renderer context");
+
+	return true;
+}
+
+update_status ModuleOpenGL::PreUpdate()
+{
+	return UPDATE_CONTINUE;
+}
+
+// Called every draw update
+update_status ModuleOpenGL::Update()
+{
+
+	return UPDATE_CONTINUE;
+}
+
+update_status ModuleOpenGL::PostUpdate()
+{
+	return UPDATE_CONTINUE;
+}
+
+// Called before quitting
+bool ModuleOpenGL::CleanUp()
+{
+	LOG("Destroying renderer");
+
+	//Destroy window
+
+	return true;
+}
+
+void ModuleOpenGL::WindowResized(unsigned width, unsigned height)
+{
+}
+
