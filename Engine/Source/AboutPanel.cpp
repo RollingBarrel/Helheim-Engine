590c6d5860974069d03c0c79f4227892634f60d1
JordiAlonsoLlovet <jordi.alonso.llovet@gmail.com>
2024-01-19 09:49:34 +0100

    Remove ImGui demo window

--- Engine/Source/AboutPanel.cpp
+++ Engine/Source/AboutPanel.cpp
@@ -12,9 +12,6 @@ AboutPanel::~AboutPanel()
 
 void AboutPanel::Draw(int windowFlags) 
 {
-	static bool show = true;
-	if (show)
-		ImGui::ShowDemoWindow(&show);
 	if (ImGui::Begin(GetName(), &mOpen, windowFlags))
 	{
 		ImGui::Text("Welcum");
