63fe8b3688024dabb7dda7d8cf6c981fe8ee6366
JordiAlonsoLlovet <jordi.alonso.llovet@gmail.com>
2024-01-20 21:05:22 +0100

    Undo changes to TestSceneGameObjects

--- Engine/Source/TestSceneGameObjects.cpp
+++ Engine/Source/TestSceneGameObjects.cpp
@@ -20,10 +20,12 @@ public:
 		LOG("TestSceneWithGameObject\n");
 		GameObject* scene = App->GetScene()->GetRoot();
 
-		GameObject* gameObject = new GameObject("GO 1", scene);
+		GameObject* gameObject = new GameObject("GO 1", nullptr);
 
 		gameObject->CreateComponent(ComponentType::MESHRENDERER);
 		//Component* newComponent = new TestComponent(gameObject);
+
+		scene->AddChild(gameObject);
 
 		return scene;
 
@@ -41,8 +43,8 @@ public:
 		Component* newComponent2 = new TestComponent(&gameObject2);
 		Component* newComponent3 = new TestComponent(&gameObject2);
 
-		/*scene->AddChild(&gameObject);
-		scene->AddChild(&gameObject2);*/
+		scene->AddChild(&gameObject);
+		scene->AddChild(&gameObject2);
 
 	}
 
@@ -57,20 +59,20 @@ public:
 	const char* TestLoadSceneWithGameObjectsAndComponents() {
 		LOG("TestLoadSceneWithGameObjectsAndComponents");
 
-		const char* json = 
+		const char* json =
 			"{\"Scene\": \n"
-				"{\"GameObjects\": [\n"
-					"{\"UID\":1, \"ParentUID\": 0, \"Name\": \"Game Object 1\", \"Translation\": [0, 1, -10], \"Rotation\": [0, 0, 0, 1], \"Scale\": [1, 1, 1],\n"
-					"\"Components\": [\n"
-						"{\"ComponentType\": 0, \"HasResource\": false},\n"
-						"{\"ComponentType\": 1, \"FOV\": 57.295780}\n"
-					"]},\n"
-					"{\"UID\":3, \"ParentUID\": 0, \"Name\": \"Game Object 2\", \"Translation\": [0, 0, 0], \"Rotation\": [0, 0, 0, 1], \"Scale\": [1, 1, 1],\n"
-					"\"Components\": [\n"
-						"{\"ComponentType\": 1, \"HasResource\": true},\n"
-						"{\"ComponentType\": 1, \"Playing\": true}\n"
-					"]}\n"
-				"]}\n"
+			"{\"GameObjects\": [\n"
+			"{\"UID\":1, \"ParentUID\": 0, \"Name\": \"Game Object 1\", \"Translation\": [0, 1, -10], \"Rotation\": [0, 0, 0, 1], \"Scale\": [1, 1, 1],\n"
+			"\"Components\": [\n"
+			"{\"ComponentType\": 0, \"HasResource\": false},\n"
+			"{\"ComponentType\": 1, \"FOV\": 57.295780}\n"
+			"]},\n"
+			"{\"UID\":3, \"ParentUID\": 0, \"Name\": \"Game Object 2\", \"Translation\": [0, 0, 0], \"Rotation\": [0, 0, 0, 1], \"Scale\": [1, 1, 1],\n"
+			"\"Components\": [\n"
+			"{\"ComponentType\": 1, \"HasResource\": true},\n"
+			"{\"ComponentType\": 1, \"Playing\": true}\n"
+			"]}\n"
+			"]}\n"
 			"}";
 
 		return json;
@@ -81,23 +83,23 @@ public:
 
 		const char* json =
 			"{\"Scene\": \n"
-				"{\"GameObjects\": [\n"
-					"{\"UID\":1, \"ParentUID\": 0, \"Name\": \"Game Object 1\", \"Translation\": [0, 1, -10], \"Rotation\": [0, 0, 0, 1], \"Scale\": [1, 1, 1],\n"
-					"\"Components\": [\n"
-						"{\"ComponentType\": 0, \"MeshID\": \"519408695\", \"MaterialID\": \"20\"},\n"
-						"{\"ComponentType\": 1, \"FOV\": 57.295780}\n"
-					"]},\n"
-					"{\"UID\":2, \"ParentUID\": 0, \"Name\": \"Game Object 2\", \"Translation\": [0, 0, 0], \"Rotation\": [0, 0, 0, 1], \"Scale\": [1, 1, 1],\n"
-					"\"Components\": [\n"
-						"{\"ComponentType\": 1, \"HasResource\": true},\n"
-						"{\"ComponentType\": 1, \"Playing\": true}\n"
-					"]},\n"
-					"{\"UID\":3, \"ParentUID\": 1, \"Name\": \"Game Object 3\", \"Translation\": [0, 1, -10], \"Rotation\": [0, 0, 0, 1], \"Scale\": [1, 1, 1],\n"
-					"\"Components\": [\n"
-						"{\"ComponentType\": 1, \"HasResource\": false},\n"
-						"{\"ComponentType\": 1, \"FOV\": 57.295780}\n"
-					"]}\n"
-				"]}\n"
+			"{\"GameObjects\": [\n"
+			"{\"UID\":1, \"ParentUID\": 0, \"Name\": \"Game Object 1\", \"Translation\": [0, 1, -10], \"Rotation\": [0, 0, 0, 1], \"Scale\": [1, 1, 1],\n"
+			"\"Components\": [\n"
+			"{\"ComponentType\": 0, \"MeshID\": \"519408695\", \"MaterialID\": \"20\"},\n"
+			"{\"ComponentType\": 1, \"FOV\": 57.295780}\n"
+			"]},\n"
+			"{\"UID\":2, \"ParentUID\": 0, \"Name\": \"Game Object 2\", \"Translation\": [0, 0, 0], \"Rotation\": [0, 0, 0, 1], \"Scale\": [1, 1, 1],\n"
+			"\"Components\": [\n"
+			"{\"ComponentType\": 1, \"HasResource\": true},\n"
+			"{\"ComponentType\": 1, \"Playing\": true}\n"
+			"]},\n"
+			"{\"UID\":3, \"ParentUID\": 1, \"Name\": \"Game Object 3\", \"Translation\": [0, 1, -10], \"Rotation\": [0, 0, 0, 1], \"Scale\": [1, 1, 1],\n"
+			"\"Components\": [\n"
+			"{\"ComponentType\": 1, \"HasResource\": false},\n"
+			"{\"ComponentType\": 1, \"FOV\": 57.295780}\n"
+			"]}\n"
+			"]}\n"
 			"}";
 
 		return json;
