e377745555a6a5cf50d1eac3825a8b36db89dd31
bottzo <45202057+bottzo@users.noreply.github.com>
2024-01-27 11:32:51 +0100

    ambient color incorrect buffer offset

--- Engine/Source/LightningPanel.cpp
+++ Engine/Source/LightningPanel.cpp
@@ -14,25 +14,25 @@ void LightningPanel::Draw(int windowFlags)
 	glUseProgram(App->GetOpenGL()->GetPBRProgramId());
 	ImGui::Begin(GetName(), &mOpen, windowFlags);
 	
-	if (ImGui::DragFloat("LightIntensity", &openGl->mDirCol[3], 0.05f, 0.0f, 5.0f))
+	if (ImGui::DragFloat("DirLIntensity", &openGl->mDirCol[3], 0.05f, 0.0f, 5.0f))
 	{
 		glBindBuffer(GL_UNIFORM_BUFFER, openGl->lightUnis);
 		glBufferSubData(GL_UNIFORM_BUFFER, sizeof(openGl->mDirDir) + sizeof(float) * 3, sizeof(float), &openGl->mDirCol[3]);
 	}
-	if (ImGui::DragFloat3("LightDir", openGl->mDirDir, 0.05f, -1.0f, 1.0f))
+	if (ImGui::DragFloat3("DirLDir", openGl->mDirDir, 0.05f, -1.0f, 1.0f))
 	{ 
 		glBindBuffer(GL_UNIFORM_BUFFER, openGl->lightUnis);
 		glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(float) * 3, App->GetOpenGL()->mDirDir);
 	}
-	if (ImGui::ColorPicker3("LightCol", openGl->mDirCol))
+	if (ImGui::ColorPicker3("DirLCol", openGl->mDirCol))
 	{ 
 		glBindBuffer(GL_UNIFORM_BUFFER, openGl->lightUnis);
 		glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(float) * 3, openGl->mDirCol);
 	}
-	if (ImGui::ColorPicker3("AmbientCol", openGl->mAmbientCol))
+	if (ImGui::ColorPicker3("AmbientLCol", openGl->mAmbientCol))
 	{ 
 		glBindBuffer(GL_UNIFORM_BUFFER, openGl->lightUnis);
-		glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(float) * 3, openGl->mAmbientCol);
+		glBufferSubData(GL_UNIFORM_BUFFER, sizeof(openGl->mDirDir) + sizeof(openGl->mDirCol), sizeof(openGl->mAmbientCol), openGl->mAmbientCol);
 	}	
 	ImGui::End();
 	glUseProgram(0);
