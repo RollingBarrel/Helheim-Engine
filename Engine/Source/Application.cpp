7fc98ca06c846c39230f90ef0bfa8c5e73f12f70
David Bardina <bardina.dev@gmail.com>
2024-02-11 18:34:02 +0100

    Test with build errors

--- Engine/Source/Application.cpp
+++ Engine/Source/Application.cpp
@@ -14,7 +14,7 @@
 Application::Application()
 {
 	// Order matters: they will Init/start/update in this order
-	//modules[0] = input = new ModuleInput();
+	modules[0] = input = new ModuleInput();
 	modules[1] = window = new ModuleWindow();
 	modules[2] = camera = new ModuleCamera();
 	modules[3] = fileSystem = new ModuleFileSystem();
@@ -34,7 +34,7 @@ Application::~Application()
 
 bool Application::Init()
 {
-	bool ret = true;
+	//bool ret = true;
 
 	for(int i = 0; i < NUM_MODULES && ret == true; ++i)
 		ret = modules[i]->Init();
