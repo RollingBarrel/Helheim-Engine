5ab3fa220ba38097e55fe8d2228e4f237515ac9f
David <dlongaron@hotmail.com>
2024-02-15 00:49:23 +0100

    feature- save navmesh foo

--- Engine/Source/ImporterNavMesh.cpp
+++ Engine/Source/ImporterNavMesh.cpp
@@ -1,6 +1,91 @@
 #include "ImporterNavMesh.h"
-
+#include <vector>
+#include "Application.h"
+#include "ModuleFileSystem.h"
 void Importer::NavMesh::Import()
 {
 	//NOT NEEDED 
+}
+
+void Importer::NavMesh::Save(const ResourceNavMesh* navMesh)
+{
+	int header[]={ navMesh->numberOfVertices, navMesh->maxPolygons, navMesh->verticesPerPolygon };
+	float cellData[] = { navMesh->cellSize,navMesh->cellHeight };
+	unsigned int totalVerticesSize = sizeof(unsigned short) * navMesh->numberOfVertices;
+	unsigned int totalPoligonsSize = sizeof(unsigned short) * navMesh->maxPolygons * 2 * navMesh->verticesPerPolygon;
+	unsigned int polygonRegionIdSize = sizeof(unsigned short) * navMesh->maxPolygons;
+	unsigned int polygonFlagsSize = sizeof(unsigned short) * navMesh->maxPolygons;
+	unsigned int polygonAreaIdSize = sizeof(unsigned short) * navMesh->maxPolygons;
+
+	unsigned int size = sizeof(header) + sizeof(cellData) + totalVerticesSize + 
+		totalPoligonsSize + polygonRegionIdSize + polygonFlagsSize + polygonAreaIdSize + 
+		sizeof(navMesh->boundMin)+ sizeof(navMesh->boundMax) + sizeof(navMesh->borderSize) + sizeof(navMesh->maxEdgeError);
+
+	char* fileBuffer = new char[size];
+	char* cursor = fileBuffer;
+
+	//Save Header
+	unsigned int bytes = sizeof(header);
+	memcpy(cursor, header, bytes);
+	cursor += bytes;
+
+	//Save Celldata
+	bytes= sizeof(cellData);
+	memcpy(cursor, cellData, bytes);
+	cursor += bytes;
+
+	//Save Vertices
+	bytes = totalVerticesSize;
+	memcpy(cursor,&navMesh->numberOfVertices, bytes);
+	cursor += bytes;
+
+	//Save Polygons
+	bytes = totalPoligonsSize;
+	memcpy(cursor, &navMesh->numberOfPolygons, bytes);
+	cursor += bytes;
+
+	//Save PolygonRegionId
+	bytes = polygonRegionIdSize;
+	memcpy(cursor, &navMesh->polygonRegionID, bytes);
+	cursor += bytes;
+
+	//Save PolygonFlags
+	bytes = polygonFlagsSize;
+	memcpy(cursor, &navMesh->polygonFlags, bytes);
+	cursor += bytes;
+
+	//Save PolygonAreaID
+	bytes = polygonAreaIdSize;
+	memcpy(cursor, &navMesh->polygonAreaID, bytes);
+	cursor += bytes;
+
+	//Save boundMin
+	bytes = sizeof(navMesh->boundMin);
+	memcpy(cursor, navMesh->boundMin, bytes);
+	cursor += bytes;
+
+	//Save boundMax
+	bytes = sizeof(navMesh->boundMax);
+	memcpy(cursor, navMesh->boundMax, bytes);
+	cursor += bytes;
+
+	//Save BorderSize
+	bytes = sizeof(navMesh->borderSize);
+	memcpy(cursor, &navMesh->borderSize, bytes);
+	cursor += bytes;
+
+	//Save MaxEdgeError
+	bytes = sizeof(navMesh->maxEdgeError);
+	memcpy(cursor, &navMesh->maxEdgeError, bytes);
+	cursor += bytes;
+
+
+	std::string path = LIBRARY_NAVMESH_PATH;
+	path += std::to_string(navMesh->mUID);
+	path += ".navmesshi";
+
+	App->GetFileSystem()->Save(path.c_str(), fileBuffer, size);
+
+	delete[] fileBuffer;
+	fileBuffer = nullptr;
 }
--- Engine/Source/ImporterNavMesh.h
+++ Engine/Source/ImporterNavMesh.h
@@ -1,8 +1,21 @@
 #pragma once
 struct ResourceNavMesh
 {
-public:
-
+	unsigned short* meshVertices;	///< The mesh vertices. [Form: (x, y, z) * #numberOfVertices]
+	unsigned short* polygons;	///< Polygon and neighbor data. [Length: #maxPolygons * 2 * #verticesPerPolygon]
+	unsigned short* polygonRegionID;	///< The region id assigned to each polygon. [Length: #maxPolygons]
+	unsigned short* polygonFlags;	///< The user defined flags for each polygon. [Length: #maxPolygons]
+	unsigned char* polygonAreaID;	///< The area id assigned to each polygon. [Length: #maxPolygons]
+	int numberOfVertices;				///< The number of vertices.
+	int numberOfPolygons;				///< The number of polygons. (Maybe not necesary having the maypolygons?)
+	int maxPolygons;			///< The number of allocated polygons.
+	int verticesPerPolygon;				///< The maximum number of vertices per polygon.
+	float boundMin[3];			///< The minimum bounds in world space. [(x, y, z)]
+	float boundMax[3];			///< The maximum bounds in world space. [(x, y, z)]
+	float cellSize;				///< The size of each cell. (On the xz-plane.)
+	float cellHeight;				///< The height of each cell. (The minimum increment along the y-axis.)
+	int borderSize;			///< The AABB border size used to generate the source data from which the mesh was derived.
+	float maxEdgeError;		///< The max error of the polygon edges in the mesh.
 };
 
 namespace Importer
@@ -10,7 +23,7 @@ namespace Importer
     namespace NavMesh
     {
         void Import();
-        void Save(const ResourceNavMesh* ourNavMesh);
+        void Save(const ResourceNavMesh* navMesh);
         void Load(ResourceNavMesh* ourNavMesh , const char* fileName = nullptr);
     }
 };
