49f9052005c9128a4014cea71080776d8779a0f1
carlosgspsp <46396509+carlosgspsp@users.noreply.github.com>
2024-02-15 18:38:26 +0100

    Squashed commit of the following:
    commit ca31dc8442ec1252b29e155fbcf3903a11004c56
    Author: bottzo <45202057+bottzo@users.noreply.github.com>
    Date:   Wed Feb 14 17:46:56 2024 +0100
    
        Lighting (#83)
    
        * point lights 0.5
    
        Added point lights on shader
        Application modules destruction inverted order
        Inspector point light component
        Removed the dynamic casts
    
        * fixed attenuation function, missed parentesis
    
        * change computer
    
        * corrected the surface position
    
        * debug draw point light
    
        * lightsourcecomponent to pointlightcomponent
    
        * added the spotlight component from the point light
    
        * change computer
    
        * corrected the pi division
    
        * change computer
    
        * change computer
    
        * added spot lights with bugs change computer
    
        * new opengl buffer object
    
        * spot light 0.9
    
        * lighting uniforms to bufferobject
    
        * change computer
    
        * changed everything to use OpenGLBuffer vlasw
    
        * archive optimization pasing by reference
    
        * lights save + load
    
        * calculate the cosinus of angles on cpu
    
        * Spot light correct behaviour
    
        * debug draw of lights
    
        * error in merge
    
    commit e3e369e0ed3fe9ec8a7923bcb23cd762e6f925b6
    Author: David Bardina <bardina.dev@gmail.com>
    Date:   Tue Feb 13 00:04:57 2024 +0100
    
        Feature/create 2 timers/86bxak1g0 (#81)
    
        * Added FPS Histogram
    
        * Improved Timer Panel
    
        Still with provisional varaibles but the structure is made
    
        * Moved variables to Timer and Application
    
        Did it in class
    
        * Commented all ModuleTimer refs
    
        Did it in class
    
        * Deleted ModuleTimer
    
        Also improved TimerPanel (needs to be finished)
    
        * Improved Timer Panel
    
        * Commented gameTimer
    
        ---------
    
        Co-authored-by: MarcGabernet <marcgr01gualter@gmail.com>
    
    commit 56ae790b25dc13dee6764af32e6c67c12e183dba
    Author: David Bardina <bardina.dev@gmail.com>
    Date:   Sun Feb 11 20:49:04 2024 +0100
    
        Create msbuild.yml to add build check on CI/CD merge requests (#80)
    
        * Create msbuild.yml
    
        * Update path
    
        * update paths
    
        * Test build with error
    
        * Test with build errors
    
        * Revert test change
    
        * check pull request merge fail when check fails build
    
        * Update file to add more conditions to trigger the build action
    
        * revert last test
    
    commit 3fab4f90c8894435189ad9f4baa676f366b88598
    Author: Oscar M <empanaisimo@gmail.com>
    Date:   Sat Feb 10 13:30:14 2024 +0100
    
        Clamp to edges (#72)

--- Engine/Source/Timer.cpp
+++ Engine/Source/Timer.cpp
@@ -11,6 +11,48 @@ void Timer::StartWithRunTime() {
 	mTotalTime = mLastReadTime;
 }
 
+void Timer::Update()
+{
+	++mUpdateFrames;
+	++mTotalFrames;
+
+	if (mChangeSpeed) {
+		mDeltaTime = SetSpeed(mNewSpeed);
+		mChangeSpeed = false;
+	}
+	else { mDeltaTime = ReadDelta(); }
+
+	if (mFpsLimit > 0 && mDeltaTime < (1000 / mFpsLimit))
+	{
+		SDL_Delay((1000 / mFpsLimit) - mDeltaTime);
+		mDeltaTime = 1000 / mFpsLimit;
+	}
+
+	if (mMsLog.size() >= 100) {
+		mMsLog.erase(mMsLog.begin());
+	}
+	mMsLog.push_back(mDeltaTime/mSpeed);
+
+	if(mSpeed != 0)
+	{
+		mUpdateTime += mDeltaTime / mSpeed;
+	}
+	else 
+	{
+		mUpdateTime += mDeltaTime;
+	}
+	if (mUpdateTime >= 500) {
+		if (mFpsLog.size() >= 100) {
+			mFpsLog.erase(mFpsLog.begin());
+		}
+		mFpsLog.push_back(mUpdateFrames * 1000 / mUpdateTime);
+		mUpdateFrames = 0;
+		mUpdateTime = 0;
+
+		mUpdateFpsLog = true;
+	}
+}
+
 long Timer::Read() {
 	ReadDelta();
 	return mTotalTime;
@@ -29,4 +71,9 @@ long Timer::SetSpeed(float speed) {
 	long currentTime = ReadDelta();
 	mSpeed = speed;
 	return currentTime;
+}
+
+void Timer::SetTimeScale(float speed) {
+	mNewSpeed = speed;
+	mChangeSpeed = true;
 }
---
+++ Engine/Source/TimerPanel.cpp
@@ -0,0 +1,108 @@
+#include "TimerPanel.h"
+#include "Application.h"
+#include "ModuleOpenGL.h"
+#include "imgui.h"
+#include "glew.h"
+
+#include "Timer.h"
+#include "PreciseTimer.h"
+
+
+TimerPanel::TimerPanel() : Panel(TIMERPANEL, true) 
+{
+}
+
+TimerPanel::~TimerPanel()
+{
+}
+
+
+void TimerPanel::Draw(int windowFlags)
+{
+	static Timer* clock = App->GetEngineClock();
+
+	ImGui::Begin(GetName(), &mOpen, windowFlags);
+
+	const char* timers[] = { "Engine", "Game"};
+	static int item_current = 0;
+	ImGui::Combo("Current Timer", &item_current, timers, 2);
+	ImGui::Text("   ");
+
+	if (item_current == 0) 
+	{
+		clock = App->GetEngineClock();
+	}
+	else 
+	{
+		clock = App->GetGameClock();
+	}
+
+	ImGui::PushItemWidth(300.0f);
+
+	static float fps;
+	static long ms;
+	static std::vector<float> fpsLog;
+	static std::vector<unsigned long> msLog;
+	
+	//Executes evey 500 ms (no change if executed every time)
+	if(clock->UpdateFpsLog())
+	{
+		fpsLog = clock->GetFpsLog();
+		clock->FpsLogUpdated();
+
+		ms = clock->GetRealDelta();
+		fps = clock->GetFPS();
+	}
+
+	int fps_limit = clock->GetFpsLimit();
+	ImGui::SliderInt("FPS Limit", &fps_limit, 10, 60);
+	clock->SetFpsLimit(fps_limit);
+
+	long framedelay = 0;
+	ImGui::Text("Last frame delayed for: %lld", framedelay);
+
+	long lowfps = 0;
+	float lowsec = 0;
+	ImGui::Text("Lowest FPS: %lld on second %.1f", lowfps, lowsec);
+
+	long slowframe = 0;
+	float slowframeN = 0;
+	ImGui::Text("Slowest frame: %lld MS on frame %.1f", slowframe, slowframeN);
+
+	float realtime = 0;
+	ImGui::Text("Real time since start: %.3f", realtime);
+
+	float realtimedt = 0;
+	ImGui::Text("Real time dt: %.3f", realtimedt);
+
+	float gametime = 0;
+	ImGui::Text("Real time since start: %.3f", gametime);
+
+	float gametimedt = 0;
+	ImGui::Text("Real time dt: %.3f", gametimedt);
+
+	if (item_current == 1 && clock->GetNewSpeed() !=0) {
+		static float gameScale;
+		gameScale = clock->GetNewSpeed();
+		ImGui::SliderFloat("Game Clock Scale", &gameScale, 0.1f, 10, "%.1f");
+		clock->SetTimeScale(gameScale);
+	}
+
+	ImGui::PopItemWidth();
+
+	ImGui::Text("Total Time: %.2f", clock->GetTotalTime()/1000.0f);
+
+	ImGui::Text("Frame Count: %u", clock->GetTotalFrames());
+
+	//Plots
+	ImGui::PlotHistogram("FPS", fpsLog.data(), fpsLog.size(), 0, NULL, FLT_MAX, FLT_MAX, ImVec2(400, 50));
+
+	msLog = clock->GetMsLog();
+	std::vector<float> msLogFloat(msLog.begin(), msLog.end());
+
+	ImGui::PlotLines("MS", msLogFloat.data(), msLogFloat.size(), 0, NULL, FLT_MAX, FLT_MAX, ImVec2(400, 50));
+
+	ImGui::Text("Application average %lld ms/frame (%.1f FPS)", ms, fps);
+
+	ImGui::End();
+}
