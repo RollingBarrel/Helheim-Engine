b033604167c6516175a4c732955dce09a5f40eb2
David <dlongaron@hotmail.com>
2024-02-17 01:58:25 +0100

    navMesh resource and  importer(save,load)

--- Engine/Source/ImporterNavMesh.h
+++ Engine/Source/ImporterNavMesh.h
@@ -1,21 +1,22 @@
 #pragma once
 struct ResourceNavMesh
 {
-	unsigned short* meshVertices;	///< The mesh vertices. [Form: (x, y, z) * #numberOfVertices]
-	unsigned short* polygons;	///< Polygon and neighbor data. [Length: #maxPolygons * 2 * #verticesPerPolygon]
-	unsigned short* polygonRegionID;	///< The region id assigned to each polygon. [Length: #maxPolygons]
-	unsigned short* polygonFlags;	///< The user defined flags for each polygon. [Length: #maxPolygons]
-	unsigned char* polygonAreaID;	///< The area id assigned to each polygon. [Length: #maxPolygons]
-	int numberOfVertices;				///< The number of vertices.
-	int numberOfPolygons;				///< The number of polygons. (Maybe not necesary having the maypolygons?)
-	int maxPolygons;			///< The number of allocated polygons.
-	int verticesPerPolygon;				///< The maximum number of vertices per polygon.
-	float boundMin[3];			///< The minimum bounds in world space. [(x, y, z)]
-	float boundMax[3];			///< The maximum bounds in world space. [(x, y, z)]
-	float cellSize;				///< The size of each cell. (On the xz-plane.)
-	float cellHeight;				///< The height of each cell. (The minimum increment along the y-axis.)
-	int borderSize;			///< The AABB border size used to generate the source data from which the mesh was derived.
-	float maxEdgeError;		///< The max error of the polygon edges in the mesh.
+	unsigned int mUID = 0;
+	unsigned short* meshVertices=nullptr;	///< The mesh vertices. [Form: (x, y, z) * #numberOfVertices]
+	unsigned short* polygons = nullptr;	///< Polygon and neighbor data. [Length: #maxPolygons * 2 * #verticesPerPolygon]
+	unsigned short* polygonRegionID = nullptr;	///< The region id assigned to each polygon. [Length: #maxPolygons]
+	unsigned short* polygonFlags = nullptr;	///< The user defined flags for each polygon. [Length: #maxPolygons]
+	unsigned char* polygonAreaID = nullptr;	///< The area id assigned to each polygon. [Length: #maxPolygons]
+	int numberOfVertices = 0;				///< The number of vertices.
+	//int numberOfPolygons = 0;				///< The number of polygons. (Maybe not necesary having the maypolygons?)
+	int maxPolygons = 0;			///< The number of allocated polygons.
+	int verticesPerPolygon = 0;				///< The maximum number of vertices per polygon.
+	float boundMin[3] = {0.0f,0.0f,0.0f };			///< The minimum bounds in world space. [(x, y, z)]
+	float boundMax[3] = { 0.0f,0.0f,0.0f };			///< The maximum bounds in world space. [(x, y, z)]
+	float cellSize = 0.0f;				///< The size of each cell. (On the xz-plane.)
+	float cellHeight = 0.0f;				///< The height of each cell. (The minimum increment along the y-axis.)
+	int borderSize = 0;			///< The AABB border size used to generate the source data from which the mesh was derived.
+	float maxEdgeError = 0.0f;		///< The max error of the polygon edges in the mesh.
 };
 
 namespace Importer
@@ -23,7 +24,7 @@ namespace Importer
     namespace NavMesh
     {
         void Import();
-        void Save(const ResourceNavMesh* navMesh);
-        void Load(ResourceNavMesh* ourNavMesh , const char* fileName = nullptr);
+        void Save(const ResourceNavMesh& navMesh);
+        void Load(ResourceNavMesh& navMesh , const char* fileName = nullptr);
     }
 };
