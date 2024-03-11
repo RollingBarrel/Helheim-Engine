fed9127e04b160ff2fac1539604215aeb43fca9b
David Bardina <bardina.dev@gmail.com>
2024-03-11 19:54:59 +0100

    last version working loading all GameObjects from the scene

--- Engine/Source/GameObject.cpp
+++ Engine/Source/GameObject.cpp
@@ -495,20 +495,16 @@ void GameObject::Save(Archive& archive) const {
 	}*/
 }
 
-GameObject* findGameObjectParent(const std::vector<GameObject*>& gameObjects, GameObject* gameObject) {
+void findGameObjectParent(const std::vector<GameObject*>& gameObjects, GameObject* gameObject) {
 	for (int i = 0; i < gameObjects.size(); i++) {
 		if (gameObjects[i]->GetID() == gameObject->GetParentID()) {
-			GameObject* go = new GameObject(gameObjects[i]->GetName().c_str(), gameObjects[i]->GetID(), gameObjects[i], gameObjects[i]->GetPositionMember(), gameObjects[i]->GetScaleMember(), gameObjects[i]->GetRotationMember());
-			gameObject->SetParentID(gameObjects[i]->GetID());
-			gameObjects[i]->AddChild(gameObject);
-			return go;
+			GameObject* go = new GameObject(gameObject->GetName().c_str(), gameObject->GetID(), gameObjects[i], gameObject->GetPositionMember(), gameObject->GetScaleMember(), gameObject->GetRotationMember());
+			go->SetParentID(gameObjects[i]->GetID());
 		}
 		else if (gameObjects[i]->GetChildren().size() != 0) {
 			findGameObjectParent(gameObjects[i]->GetChildren(), gameObject);
 		}
 	}
-
-	return nullptr;
 }
 
 void loadComponentsFromJSON(const rapidjson::Value& components, GameObject* go) {
@@ -614,16 +610,11 @@ void GameObject::Load(const rapidjson::Value& sceneJson) {
 		}
 		
 		for (int i = 0; i < gameObjects.size(); i++) {
-			GameObject* go = nullptr;
 			if (gameObjects[i]->GetParentID() == 1) {
-				go = new GameObject(gameObjects[i]->GetName().c_str(), gameObjects[i]->GetID(), scene, gameObjects[i]->GetPositionMember(), gameObjects[i]->GetScaleMember(), gameObjects[i]->GetRotationMember());
+				GameObject* go = new GameObject(gameObjects[i]->GetName().c_str(), gameObjects[i]->GetID(), scene, gameObjects[i]->GetPositionMember(), gameObjects[i]->GetScaleMember(), gameObjects[i]->GetRotationMember());
 			}
 			else {
-				GameObject* tmpGo = findGameObjectParent(scene->GetChildren(), gameObjects[i]);
-				if (go != nullptr) {
-					go = new GameObject(tmpGo->GetName().c_str(), tmpGo->GetID(), gameObjects[i], tmpGo->GetPositionMember(), tmpGo->GetScaleMember(), tmpGo->GetRotationMember());
-				}
-
+				findGameObjectParent(scene->GetChildren(), gameObjects[i]);
 			}
 		}
 	}
