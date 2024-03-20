cef2c007fe1010fc8731c8598fad7b1f3aebbe4d
carlosgspsp <46396509+carlosgspsp@users.noreply.github.com>
2024-03-21 00:30:02 +0100

    HUD (#178)

--- Engine/Source/ModuleUI.cpp
+++ Engine/Source/ModuleUI.cpp
@@ -59,12 +59,12 @@ bool ModuleUI::Init() {
 
 update_status ModuleUI::PreUpdate(float dt) {
 	//if (mScreenSpace) {
-	//	mCurrentFrustum = mUIfrustum;
-	//	glEnable(GL_DEPTH_TEST);
+		mCurrentFrustum = mUIfrustum;
+		glEnable(GL_DEPTH_TEST);
 	//}
 	//else {
-		mCurrentFrustum = (Frustum*)(App->GetCamera()->GetFrustum());
-		glDisable(GL_DEPTH_TEST);
+		//mCurrentFrustum = (Frustum*)(App->GetCamera()->GetFrustum());
+		//glDisable(GL_DEPTH_TEST);
 	//}
 
 	// Draw the UI
