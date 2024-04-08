521f989c961a6d533a1608dff378f1c57715a75b
Oscar Pellice Mas <58166158+Oscar-Pellice-Mas@users.noreply.github.com>
2024-04-08 11:17:00 +0200

    Squashed commit of the following:
    commit 46c4d51bcc223a248e52dfc3716b491f64809279
    Author: Oscar M. <empanaisimo@gmail.com>
    Date:   Sun Apr 7 21:53:11 2024 +0200
    
        Add drag&drop and texture preview in inspector panel
    
    commit 93178a8bd02d8b8b57a45e1b32c75c270719a3f6
    Author: Oscar M. <empanaisimo@gmail.com>
    Date:   Sun Apr 7 10:47:40 2024 +0200
    
        CU-86by6nqrt - Image
    
    commit 9dd32b575fbc5d88a2125d8e84f8580cd11aab46
    Author: Oscar M. <empanaisimo@gmail.com>
    Date:   Sun Apr 7 10:36:15 2024 +0200
    
        Add color and alpha in inspector panel
    
    commit 245511cf0cc31d8ae1e2a33d084074bcd39e4615
    Author: Oscar M. <empanaisimo@gmail.com>
    Date:   Sat Apr 6 20:59:31 2024 +0200
    
        Add ModulePrograms, refactor ImageComponent and refactor

--- Engine/Source/ImageComponent.cpp
+++ Engine/Source/ImageComponent.cpp
@@ -5,6 +5,7 @@
 #include "ModuleResource.h"
 #include "ModuleWindow.h"
 #include "ModuleUI.h"
+#include "ModulePrograms.h"
 
 #include "glew.h"
 #include "ResourceTexture.h"
@@ -25,68 +26,78 @@ ImageComponent::ImageComponent(GameObject* owner) : Component(owner, ComponentTy
 }
 
 ImageComponent:: ~ImageComponent() {
+	CleanUp();
 }
 
-void ImageComponent::Draw() const
+void ImageComponent::Draw()
 {
-    unsigned int program = App->GetUI()->GetProgram();
-    if (program)
-    {
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    unsigned int UIImageProgram= App->GetPrograms()->GetUIImageProgram();
+    if (UIImageProgram == 0) return;
 
-        glUseProgram(program);
+	if (hasAlpha) {
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	}
 
-        float4x4 proj = App->GetUI()->GetFrustum()->ProjectionMatrix();
-        float4x4 model;
+	glUseProgram(UIImageProgram);
 
-        if (App->GetUI()->GetScreenSpace()) {  //Ortographic Mode
-            /*float3 OwnerTranslation = GetOwner()->GetPosition();
+	float4x4 proj = App->GetUI()->GetFrustum()->ProjectionMatrix();
+	float4x4 model;
 
-            float3 scaleImage = GetOwner()->GetScale();
-            float scaleFloatX = scaleImage.x;
-            float scaleFloatY = scaleImage.y;
-            float scaledWidth = scaleFloatX * GetImage()->GetWidth();
-            float scaledHeight = scaleFloatY * GetImage()->GetHeight();
+	if (App->GetUI()->GetScreenSpace()) {  //Ortographic Mode
+		/*float3 OwnerTranslation = GetOwner()->GetPosition();
 
-            model = float4x4::FromTRS(OwnerTranslation, Quat::identity, float3(scaledWidth, scaledHeight, 1.0f));*/
-            Transform2DComponent* component = reinterpret_cast<Transform2DComponent*>(GetOwner()->GetComponent(ComponentType::TRANSFORM2D));
-            if (component != nullptr) 
-            {
-                model = component->GetGlobalMatrix();
-            }
-            else 
-            {
-                model = float4x4::identity;
-            }
-        }
-        else //World Mode
-        {
-            model = GetOwner()->GetWorldTransform();
-        }
+		float3 scaleImage = GetOwner()->GetScale();
+		float scaleFloatX = scaleImage.x;
+		float scaleFloatY = scaleImage.y;
+		float scaledWidth = scaleFloatX * GetImage()->GetWidth();
+		float scaledHeight = scaleFloatY * GetImage()->GetHeight();
 
-        //float4x4 model = GetOwner()->GetWorldTransform();
+		model = float4x4::FromTRS(OwnerTranslation, Quat::identity, float3(scaledWidth, scaledHeight, 1.0f));*/
+		Transform2DComponent* component = reinterpret_cast<Transform2DComponent*>(GetOwner()->GetComponent(ComponentType::TRANSFORM2D));
+		if (component != nullptr) 
+		{
+			model = component->GetGlobalMatrix();
+		}
+		else 
+		{
+			model = float4x4::identity;
+		}
+	}
+	else //World Mode
+	{
+		model = GetOwner()->GetWorldTransform();
+	}
 
-        float4x4 view = App->GetUI()->GetFrustum()->ViewMatrix();
+	//float4x4 model = GetOwner()->GetWorldTransform();
 
-        glBindVertexArray(App->GetUI()->GetQuadVAO());
+	float4x4 view = App->GetUI()->GetFrustum()->ViewMatrix();
 
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, mImage->GetOpenGLId());
+	FillVBO();
+	CreateVAO();
 
-        glUniformMatrix4fv(0, 1, GL_TRUE, &model[0][0]);
-        glUniformMatrix4fv(1, 1, GL_TRUE, &view[0][0]);
-        glUniformMatrix4fv(2, 1, GL_TRUE, &proj[0][0]);
+	glBindVertexArray(mQuadVAO);
 
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+	glActiveTexture(GL_TEXTURE0);
 
-        glBindTexture(GL_TEXTURE_2D, 0);
-        glBindVertexArray(0);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	glUniform4fv(glGetUniformLocation(UIImageProgram, "inputColor"), 1, float4(mColor, mAlpha).ptr());
+	//glUniform1i(glGetUniformLocation(UIImageProgram, "hasDiffuse"), mHasDiffuse);
+	//glUniform2fv(glGetUniformLocation(UIImageProgram, "offSet"), 1, mTexOffset.ptr());
 
-        glUseProgram(0);
-        glDisable(GL_BLEND);
-    }
+	glBindTexture(GL_TEXTURE_2D, mImage->GetOpenGLId());
+
+	glUniformMatrix4fv(0, 1, GL_TRUE, &model[0][0]);
+	glUniformMatrix4fv(1, 1, GL_TRUE, &view[0][0]);
+	glUniformMatrix4fv(2, 1, GL_TRUE, &proj[0][0]);
+
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	glUseProgram(0);
+	glDisable(GL_BLEND);
 }
 
 Component* ImageComponent::Clone(GameObject* owner) const
@@ -118,4 +129,41 @@ void ImageComponent::LoadFromJSON(const rapidjson::Value& data, GameObject* owne
 
 void ImageComponent::SetImage(unsigned int resourceId) {
     mImage = (ResourceTexture*)App->GetResource()->RequestResource(resourceId, Resource::Type::Texture);
+}
+
+void ImageComponent::FillVBO()
+{
+	float vertices[] = {
+		// texture coordinates
+		-0.5f,  0.5f,  0.0f,  0.0f,   // top-left vertex
+		-0.5f, -0.5f,  0.0f,  1.0f,   // bottom-left vertex
+		0.5f, -0.5f,  1.0f,  1.0f,   // bottom-right vertex
+		0.5f,  0.5f,  1.0f,  0.0f,   // top-right vertex
+		-0.5f,  0.5f,  0.0f,  0.0f,   // top-left vertex
+		0.5f, -0.5f,  1.0f,  1.0f    // bottom-right vertex
+	};
+	glGenBuffers(1, &mQuadVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, mQuadVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
+}
+
+void ImageComponent::CreateVAO()
+{
+	glGenVertexArrays(1, &mQuadVAO);
+	glBindVertexArray(mQuadVAO);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+
+	glBindVertexArray(0);
+}
+
+bool ImageComponent::CleanUp()
+{
+	glDeleteBuffers(1, &mQuadVBO);
+	glDeleteVertexArrays(1, &mQuadVAO);
+	return true;
 }
