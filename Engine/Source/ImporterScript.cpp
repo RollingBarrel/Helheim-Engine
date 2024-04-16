9b223130c566917f13f74cd0793b597c13595c04
Akiramei11 <mbelmontej@gmail.com>
2024-04-16 13:50:35 +0200

    clean resource script

--- Engine/Source/ImporterScript.cpp
+++ Engine/Source/ImporterScript.cpp
@@ -10,19 +10,9 @@
 #include "ImporterScript.h"
 #include "ResourceScript.h"
 
-std::string trim(const std::string& str, const char* errasable) {
-    size_t first = str.find_first_not_of(errasable);
-    if (std::string::npos == first) {
-        return str;
-    }
-    size_t last = str.find_last_not_of(errasable);
-    return str.substr(first, (last - first + 1));
-}
-
 // Function to parse a header file and extract attribute names and types
 bool parseHeaderFile(const std::string& filename, ResourceScript* rScript)
 {
-
     std::ifstream file(filename);
     if (!file.is_open())
     {
@@ -50,70 +40,6 @@ bool parseHeaderFile(const std::string& filename, ResourceScript* rScript)
     }
     rScript->SetScriptName(scriptName);
 
-    // Init a ScriptAttribute
-    ScriptAttribute attribute = ScriptAttribute();
-    bool serializable = true;
-
-    // Read line by line in the .h file
-    while (std::getline(file, line))
-    {
-        std::string trimmedLine = trim(line, " \t"); // get rid of white spaces
-        std::string firstWord;
-        size_t firstSpacePos = trimmedLine.find(' ');
-        if (firstSpacePos != std::string::npos)
-        {
-            firstWord = trimmedLine.substr(0, firstSpacePos);
-        }
-        else {
-            firstWord = trimmedLine;
-        }
-     
-        // if this line has a declaration of a variable
-        if (trimmedLine.find(';') != std::string::npos and trimmedLine.find('(') == std::string::npos)
-        {
-            trimmedLine = trimmedLine.substr(0, trimmedLine.find(';'));
-            std::istringstream iss(trimmedLine);
-
-            // Extract the variable type
-            std::string type;
-            if (!(iss >> type))
-            {
-                // se esperaba el tipo de la variable
-                return false;
-            }
-            // when there is a keyword before the type extract the type again
-            if (type == "const")
-            {
-                if (!(iss >> type))
-                {
-                    // se esperaba el tipo de la variable
-                    return false;
-                }
-            }
-            // try to convert the 'type' to a ScriptAttributeType and set it in the ScriptAttribute
-            if (!attribute.setTypeFromString(type))
-            {
-                // el tipo de la variable no es valido
-                return false;
-            }
-            // Extract the variable name
-            std::string name;
-            if (!(iss >> name))
-            {
-                // se esperaba el nombre de la variable
-                return false;
-            }
-            attribute.setName(name.c_str());
-
-            if (serializable)
-            {
-                rScript->addAttribute(attribute);
-            }
-            attribute = ScriptAttribute();
-            bool serializable = true;
-        }
-    }
-
     return true;
 }
 
@@ -129,45 +55,20 @@ ResourceScript* Importer::Script::Import(const char* filePath, unsigned int uid)
 void Importer::Script::Save(const ResourceScript* rScript)
 {
     size_t sizeScriptName = rScript->GetScriptName().length();
-    size_t sizeAttributes = rScript->GetAttributes().size();
-    size_t sizeScriptAttributes = 0;
-    for (size_t i = 0; i < sizeAttributes; ++i)
-    {
-        sizeScriptAttributes += rScript->GetAttributes()[i].sizeOfScriptVariable();
-    }
 
     // size of all the data to be saved
-    size_t size = sizeof(size_t) * 3 +
-        sizeScriptName * sizeof(char) +
-        sizeScriptAttributes;
+    size_t size = sizeof(size_t) +
+        sizeScriptName * sizeof(char); 
     char* fileBuffer = new char[size];
     char* cursor = fileBuffer;
 
     size_t bytes = sizeof(size_t);
     memcpy(cursor, &sizeScriptName, bytes);
     cursor += bytes;
-    memcpy(cursor, &sizeAttributes, bytes);
-    cursor += bytes;
 
     bytes = sizeScriptName * sizeof(char);
     memcpy(cursor, rScript->GetScriptName().c_str(), bytes);
     cursor += bytes;
-
-    ScriptAttribute attribute;
-    for (size_t i = 0; i < sizeAttributes; ++i)
-    {
-        attribute = rScript->GetAttributes()[i];
-        size_t nameSizeInBytes = attribute.GetName().size() * sizeof(char);
-        size_t sizeTSizeInBites = sizeof(size_t);
-        memcpy(cursor, &nameSizeInBytes, sizeTSizeInBites);
-        cursor += sizeTSizeInBites;
-        memcpy(cursor, attribute.GetName().c_str(), nameSizeInBytes);
-        cursor += nameSizeInBytes;
-        size_t typeSizeInBytes = sizeof(MemberType);
-        MemberType type = attribute.GetType();
-        memcpy(cursor, &type, typeSizeInBytes);
-        cursor += typeSizeInBytes;
-    }
 
     const char* libraryPath = App->GetFileSystem()->GetLibraryFile(rScript->GetUID(), true);
     App->GetFileSystem()->Save(libraryPath, fileBuffer, size);
@@ -183,7 +84,7 @@ ResourceScript* Importer::Script::Load(const char* filePath, unsigned int uid)
 
     if (App->GetFileSystem()->Load(filePath, &fileBuffer))
     {
-       /* char* cursor = fileBuffer;
+       char* cursor = fileBuffer;
 
         size_t sizeTBytes = sizeof(size_t);
 
@@ -191,37 +92,11 @@ ResourceScript* Importer::Script::Load(const char* filePath, unsigned int uid)
         memcpy( &sizeScriptName, cursor, sizeTBytes);
         cursor += sizeTBytes;
 
-        size_t sizeAttributes;
-        memcpy( &sizeAttributes, cursor, sizeTBytes);
-        cursor += sizeTBytes;
-
         std::string scriptName;
         memcpy(&scriptName, cursor, sizeScriptName);
         cursor += sizeScriptName;
         rScript->SetScriptName(scriptName);
 
-        for (size_t i = 0; i < sizeAttributes; ++i)
-        {
-            ScriptAttribute attribute;
-
-            size_t nameSizeInBytes;
-            size_t sizeTSizeInBites = sizeof(size_t);
-            memcpy(&nameSizeInBytes, cursor, sizeTSizeInBites);
-            cursor += sizeTSizeInBites;
-            char* name = new char[nameSizeInBytes];
-            memcpy(name, cursor, nameSizeInBytes);
-            cursor += nameSizeInBytes;
-            attribute.setName(name);
-
-            size_t typeSizeInBytes = sizeof(MemberType);
-            MemberType type;
-            memcpy(&type, cursor, typeSizeInBytes);
-            cursor += typeSizeInBytes;
-            attribute.setType(type);
-
-            rScript->addAttribute(attribute);
-        }
-        */
         delete[] fileBuffer;
     }
     
--- Engine/Source/ResourceScript.h
+++ Engine/Source/ResourceScript.h
@@ -3,23 +3,6 @@
 #include "Script.h"
 #include <vector>
 
-class ScriptAttribute 
-{
-public:
-	ScriptAttribute() {}
-	ScriptAttribute(std::string name, MemberType type, void* data) : mName(name), mType(type) {}
-	bool setTypeFromString(const std::string& type);
-	void setName(const std::string name);
-	void setType(const MemberType type);
-	size_t sizeOfScriptVariable() const;
-	const std::string GetName() const { return mName; }
-	const MemberType GetType() const { return mType; }
-
-private:
-	std::string mName;
-	MemberType mType = MemberType::NONE;
-
-};
 
 class ResourceScript : public Resource
 {
@@ -27,15 +10,11 @@ public:
 	ResourceScript(unsigned int uid);
     ~ResourceScript();
 
-	void addAttribute(const ScriptAttribute& attribute);
-
-	const std::vector<ScriptAttribute>& GetAttributes() const { return mAttributes; }
 	const std::string& GetScriptName() const { return mScriptName; }
 
 	void SetScriptName(const std::string& scriptName) { mScriptName = scriptName; }
 
 private:
 	std::string mScriptName;
-	std::vector<ScriptAttribute> mAttributes;
 };
 
