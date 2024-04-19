a757201a5612991e9f69aa7492c9284f0bd5abc8
JordiAlonsoLlovet <jordi.alonso.llovet@gmail.com>
2024-04-19 18:00:44 +0200

    shader

--- Engine/Game/Assets/Shaders/particle_vertex.glsl
+++ Engine/Game/Assets/Shaders/particle_vertex.glsl
@@ -1,11 +1,11 @@
 #version 460 core
 layout (location = 0) in vec4 vertex; // <vec2 position, vec2 texCoords>
+layout (location = 1) in mat4 matrix;
 
 out vec2 TexCoords;
 out vec4 ParticleColor;
 
 uniform mat4 projection;
-uniform vec3 offset;
 uniform vec4 color;
 
 void main()
@@ -13,5 +13,5 @@ void main()
     float scale = 1.0f;
     TexCoords = vertex.zw;
     ParticleColor = color;
-    gl_Position = projection * vec4((vertex.xy * scale) + offset.xy, offset.z, 1.0);
+    gl_Position = matrix * projection * vec4((vertex.xy * scale) + offset.xy, offset.z, 1.0);
 }
