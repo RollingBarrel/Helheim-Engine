a522ee6ed7bdd0d809f0fefb24843cb3eeb9c790
Oscar M. <empanaisimo@gmail.com>
2024-04-20 21:57:03 +0200

    Add fill

--- Engine/Source/SliderComponent.cpp
+++ Engine/Source/SliderComponent.cpp
@@ -21,7 +21,6 @@ void SliderComponent::Init()
 	GameObject* self = (GameObject*)this->GetOwner();
 	self->SetName("Slider");
 
-	//GameObject* background = new GameObject("Background", self);
 	GameObject* fill = new GameObject("Fill", self);
 
 	Transform2DComponent* sliderTransform2D = new Transform2DComponent(this->GetOwner());
@@ -37,12 +36,12 @@ void SliderComponent::Init()
 	// Set default values
 	bgImage->SetColor(float3(1.f, 0, 0));
 	fillImage->SetColor(float3(0, 1.f, 0));
-	//sliderTransform2D->SetPosition(float3(750, -500, 0));
 	sliderTransform2D->SetSize(float2(400, 50));
 
-	float3 bgPosition = sliderTransform2D->GetPosition();
+	const float3 bgPosition = sliderTransform2D->GetPosition();
+	const float3 fillPosition = float3(bgPosition.x-(fillTransform2D->GetSize().x)/2, bgPosition.y, bgPosition.z);
 
-	fillTransform2D->SetPosition()
+	fillTransform2D->SetPosition(fillPosition);
 }
 
 
