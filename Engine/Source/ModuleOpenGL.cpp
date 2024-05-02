48a439ab308cba7c32eb305b6e3129784ec4f6fd
bottzo <45202057+bottzo@users.noreply.github.com>
2024-05-02 17:52:01 +0200

    clamp to edge

--- Engine/Source/ModuleOpenGL.cpp
+++ Engine/Source/ModuleOpenGL.cpp
@@ -637,6 +637,7 @@ void ModuleOpenGL::BakeIBL(const wchar_t* hdrTexPath)
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RG16F, 512, 512, 0, GL_RG, GL_FLOAT, NULL);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		glUseProgram(mSpecEnvBRDFProgramId);
