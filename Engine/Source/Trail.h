f6d9de6ff175d700668fcb9ded502a09f3fa1a06
Akiramei11 <mbelmontej@gmail.com>
2024-05-09 16:58:02 +0200

    Trails Starting point

---
+++ Engine/Source/Trail.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <deque>
+#include "MathGeoLib.h"
+#include "ColorGradient.h"
+
+class TrailPoint
+{
+public:
+	TrailPoint();
+	~TrailPoint();
+
+
+private:
+	float3 mPosition;
+	float3 mDirection;
+	float mLifeTime;
+};
+
+class Trail
+{
+public:
+	Trail();
+	~Trail();
+
+	void AddTrailPositions(float3 position, float3 rotation);
+
+private:
+	std::deque<TrailPoint> mPoints;
+	float mMaxLifeTime;
+	float mWidth;
+	ColorGradient mGradient;
+	float3 mDirection; // Fixed direction for the normal of the trailPoints
+};
