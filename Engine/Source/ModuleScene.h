82fb3227a9cd1d9705823b06e1c4c5e046c5080a
Oscar Pellice Mas <58166158+Oscar-Pellice-Mas@users.noreply.github.com>
2024-05-13 14:16:39 +0200

    Bugfix/scene go (#248)
    * Add and delete scene objects from constructor
    
    * Scene save is iterative
    
    * Fix on duplicates
    
    * Brackets

--- Engine/Source/ModuleScene.h
+++ Engine/Source/ModuleScene.h
@@ -31,6 +31,10 @@ public:
 	// GameObjects
 	GameObject* Find(const char* name) const;
 	GameObject* Find(unsigned int UID) const;
+	void AddGameObjectToScene(GameObject* gameObject);
+	void RemoveGameObjectFromScene(GameObject* gameObjet);
+	void RemoveGameObjectFromScene(int id); 
+	void RemoveGameObjectFromScene(const std::string& name);
 	void AddGameObjectToDelete(GameObject* gameObject) {
 		mGameObjectsToDelete.push_back(gameObject);
 	}
@@ -86,7 +90,7 @@ private:
 	void LoadGameObjectsIntoScripts();
 	
 	void SaveGame(const std::vector<GameObject*>& gameObjects, Archive& rootArchive) const;
-	void SaveGameObjectRecursive(const GameObject* gameObject, std::vector<Archive>& gameObjectsArchive, int parentUuid) const;
+	void SaveGameObjectRecursive(const GameObject* gameObject, std::vector<Archive>& gameObjectsArchive) const;
 	void LoadGameObject(const rapidjson::Value& gameObjectsJson, GameObject* parent);
 
 	GameObject* mRoot = nullptr;
