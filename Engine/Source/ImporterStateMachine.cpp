abdd4a6089f08b011d41d44716ec96222ce810ac
OriolGallego2001 <oriol.gallego01@estudiant.upf.edu>
2024-06-06 10:56:39 +0200

    Importer create meta and lib

--- Engine/Source/ImporterStateMachine.cpp
+++ Engine/Source/ImporterStateMachine.cpp
@@ -1,9 +1,29 @@
 #include "ImporterStateMachine.h"
 #include "ResourceStateMachine.h"
 #include "AnimationStateMachine.h"
+#include "EngineApp.h"
+#include "ModuleEngineResource.h"
+#include "ModuleFileSystem.h"
+
+
 ResourceStateMachine* Importer::StateMachine::Import(const char* assetsPath, unsigned int uid)
 {
     // Copy assets path into correct library path
+    AnimationStateMachine* newSM = new AnimationStateMachine();
+    newSM->LoadResource(assetsPath);
+    ResourceStateMachine* newResource = new ResourceStateMachine(uid, newSM);
 
-    return nullptr;
+
+	const char* libraryFile = EngineApp->GetFileSystem()->GetLibraryFile(uid);
+
+
+	char* fileBuffer = nullptr;
+	int size = App->GetFileSystem()->Load(assetsPath, &fileBuffer);
+	std::string path = App->GetFileSystem()->GetLibraryFile(uid, true);
+	App->GetFileSystem()->Save(path.c_str(), fileBuffer, size);
+	
+
+    EngineApp->GetEngineResource()->CreateAssetsMeta(*newResource, assetsPath);
+
+    return newResource;
 }
