620a446e7c14bfa38ae40329c4394c1b17c1a3e9
OriolGallego2001 <oriol.gallego01@estudiant.upf.edu>
2024-06-06 10:30:12 +0200

    Create importer resource

---
+++ Engine/Source/ImporterStateMachine.cpp
@@ -0,0 +1,9 @@
+#include "ImporterStateMachine.h"
+#include "ResourceStateMachine.h"
+#include "AnimationStateMachine.h"
+ResourceStateMachine* Importer::StateMachine::Import(const char* assetsPath, unsigned int uid)
+{
+    // Copy assets path into correct library path
+
+    return nullptr;
+}
