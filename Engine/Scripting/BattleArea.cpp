436d8831de24f1183044f9d26dd04d029ecb62c1
David <dlongaron@hotmail.com>
2024-06-07 10:35:05 +0200

    created battle area

---
+++ Engine/Scripting/BattleArea.cpp
@@ -0,0 +1,22 @@
+#include "BattleArea.h"
+
+
+CREATE(BattleArea)
+{
+    CLASS(owner);
+
+    SEPARATOR("Spawner");
+    MEMBER(MemberType::GAMEOBJECT, mSpawner1);
+    MEMBER(MemberType::GAMEOBJECT, mSpawner2);
+    MEMBER(MemberType::GAMEOBJECT, mSpawner3);
+    MEMBER(MemberType::GAMEOBJECT, mSpawner4);
+
+}
+
+BattleArea::BattleArea(GameObject* owner): Script(owner)
+{
+}
+
+BattleArea::~BattleArea()
+{
+}
---
+++ Engine/Scripting/BattleArea.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <Script.h>
+#include "Macros.h"
+
+GENERATE_BODY(BattleArea);
+class BattleArea : public Script
+{
+	FRIEND(BattleArea)
+public:
+	BattleArea(GameObject* owner);
+	~BattleArea();
+	void Start() override;
+	void Update() override;
+
+
+private:
+	int mMaxNumEnemies = 0;
+	int mCurrentEnemies = 0;
+	GameObject* mSpawner1 = nullptr;
+	GameObject* mSpawner2 = nullptr;
+	GameObject* mSpawner3 = nullptr;
+	GameObject* mSpawner4 = nullptr;
+};
+
