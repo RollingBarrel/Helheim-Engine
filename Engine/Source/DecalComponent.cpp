354a2716c06672795657d6b7818c8cb4d81c5f9b
carlosgspsp <46396509+carlosgspsp@users.noreply.github.com>
2024-06-07 19:45:24 +0200

    Decals Save And Load

--- Engine/Source/DecalComponent.cpp
+++ Engine/Source/DecalComponent.cpp
@@ -1,4 +1,9 @@
 #include "DecalComponent.h"
+#include "Application.h"
+
+#include "ModuleResource.h"
+
+#include "ResourceTexture.h"
 
 DecalComponent::DecalComponent(GameObject* owner) : Component(owner, ComponentType::DECAL)
 {
@@ -26,9 +31,57 @@ Component* DecalComponent::Clone(GameObject* owner) const
 void DecalComponent::Save(JsonObject& obj) const
 {
 	Component::Save(obj);
+
+	if (mDiffuseTexture)
+	{
+		obj.AddInt("DiffuseID", mDiffuseTexture->GetUID());
+	}
+	obj.AddString("DiffuseName", mDiffuseName.c_str());
+	
+	if (mSpecularTexture)
+	{
+		obj.AddInt("SpecularID", mSpecularTexture->GetUID());
+	}
+	obj.AddString("SpecularName", mSpecularName.c_str());
+
+	if (mNormalTexture)
+	{
+		obj.AddInt("NormalID", mNormalTexture->GetUID());
+	}
+	obj.AddString("NormalName", mNormalName.c_str());
+
 }
 
 void DecalComponent::Load(const JsonObject& data)
 {
 	Component::Load(data);
+	
+	if (data.HasMember("DiffuseID"))
+	{
+		(ResourceTexture*)App->GetResource()->RequestResource(data.GetInt("DiffuseID"), Resource::Type::Texture);
+		if (data.HasMember("DiffuseName"))
+		{
+			mDiffuseName = data.GetString("DiffuseName");
+		}
+	}
+	
+	if (data.HasMember("SpecularID"))
+	{
+		(ResourceTexture*)App->GetResource()->RequestResource(data.GetInt("SpecularID"), Resource::Type::Texture);
+		if (data.HasMember("SpecularName"))
+		{
+			mDiffuseName = data.GetString("SpecularName");
+		}
+	}
+	
+	if (data.HasMember("NormalID"))
+	{
+		(ResourceTexture*)App->GetResource()->RequestResource(data.GetInt("NormalID"), Resource::Type::Texture);
+		if (data.HasMember("NormalName"))
+		{
+			mDiffuseName = data.GetString("NormalName");
+		}
+	}
+
+	
 }
