bdeb10158ed8f136a7a8878ce0f7dcb68499b349
carlosgspsp <46396509+carlosgspsp@users.noreply.github.com>
2024-06-20 00:37:07 +0200

    ObjectPool

--- Engine/Scripting/ObjectPool.cpp
+++ Engine/Scripting/ObjectPool.cpp
@@ -2,96 +2,40 @@
 #include "Application.h"
 #include "ModuleScene.h"
 #include "GameObject.h"
-#include "Bullet.h"
-#include "ScriptComponent.h"
 
-
-CREATE(ObjectPool)
+ObjectPool::ObjectPool(const char* name, unsigned int size)
 {
-    CLASS(owner);
+	mObjects.reserve(size);
+	GameObject* prefab = App->GetScene()->InstantiatePrefab(name);
+	mObjects.push_back(prefab);
 
-    END_CREATE;
-}
-
-ObjectPool::ObjectPool(GameObject* owner) : Script(owner) {}
-
-void ObjectPool::Start()
-{
-    //TAG: App->GetScene()->FindGameObjectsWithTag(App->GetScene()->GetTagByName("Bullet")->GetID(), mObject);
-    mPoolCount = mObject.size();
-    for (int i = 0 ; i < mObject.size() ; i++)
-    {
-        ScriptComponent* script = (ScriptComponent*)mObject[i]->GetComponent(ComponentType::SCRIPT);
-        //((Bullet*)script->GetScriptInstance())->objectPool = this;
-		mObject[i]->SetEnabled(false);
+	for (unsigned int i = 1; i < mObjects.size(); ++i)
+	{
+		mObjects.push_back(new GameObject(*prefab, App->GetScene()->GetRoot()));
 	}
 }
 
-GameObject* ObjectPool::GetPooledObject()
+ObjectPool::~ObjectPool()
 {
-    // If the pool count reaches zero, bad
-    if (mObject.size() <= 0)
-    {
-        return nullptr;
-    }
-
-    for (int i = 0; i < mObject.size(); i++)
-    {
-        mObject[i]->SetEnabled(true);
-        
-        GameObject* goToReturn = mObject[i];
-
-        ScriptComponent* script = (ScriptComponent*)mObject[i]->GetComponent(ComponentType::SCRIPT);
-        //((Bullet*)script->GetScriptInstance())->mTimePassed = 0;
-
-        Pop(mObject,i);
-
-        //mPoolCount--;
-        return goToReturn;
-    }
-    
-    return nullptr;
 }
 
-void ObjectPool::ReturnToPool(GameObject* go) {
-    go->SetEnabled(false);
-    mObject.push_back(go);
-}
-
-void ObjectPool::Update()
+GameObject* ObjectPool::GetObject()
 {
-    /*for (int i = 0; i < mObject.size(); i++)
-    {
-        if (mObject[i]->IsEnabled())
-        {
-            mObject[i]->Update();
+	for (unsigned int i = 0; i < mObjects.size(); ++i)
+	{
+		if (!mObjects[i]->IsEnabled())
+		{
+			mObjects[i]->SetEnabled(true);
+			return mObjects[i];
 		}
-	}*/
-}
+	}
 
-/*void ObjectPool::RefillPool()
-{
-    for (int i = 0; i < mObject.size(); i++)
-    {
-        mObject[i]->SetEnabled(false); // Disable all objects in the pool
-    }
-    mPoolCount = mObject.size(); // Reset pool count
-}*/
-
-void ObjectPool::Pop(std::vector<GameObject*>& vec, size_t index) {
-    if (index >= vec.size()) {
-        return;
-    }
-
-    vec.erase(vec.begin() + index); // Remove the element at the given index
-
-    // Shift the remaining elements to fill the vacated space
-    //for (size_t i = index; i < vec.size(); ++i) {
-    //    vec[i] = vec[i + 1];
-    //}
-    ///vec.pop_back(); // Remove the last element (which is now duplicated at the end)
-}
-
-void ObjectPool::RefillPool()
-{
+	if (!mObjects.empty())
+	{
+		assert("MORE OBJECTS NEEDED IN THE POOL");
+		GameObject* newGameObject = new GameObject(*mObjects[0], App->GetScene()->GetRoot());
+		mObjects.push_back(newGameObject);
+		return newGameObject;
+	}
+	return nullptr;
 }
