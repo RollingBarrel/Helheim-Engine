a319e45f6f3b5ecc9641584a1e180340f4392f7c
Aileck <61742408+Aileck@users.noreply.github.com>
2024-06-20 20:29:59 +0200

    create new class

---
+++ Engine/Scripting/ElectricTrapController.cpp
@@ -0,0 +1,5 @@
+#include "ElectricTrapController.h"
+
+ElectricTrapController::ElectricTrapController(GameObject* owner) : GameObject(owner)
+{
+}
---
+++ Engine/Scripting/ElectricTrapController.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <Script.h>
+
+struct CollisionData;
+class BoxColliderComponent;
+
+class ElectricTrapController :
+    public Script
+{
+public:
+	ElectricTrapController(GameObject* owner);
+	~ElectricTrapController() {}
+	void Update() override;
+	void Start() override;
+private:
+	void OnCollisionEnter(CollisionData* collisionData);
+
+	BoxColliderComponent* mCollider = nullptr;
+
+
+};
+
