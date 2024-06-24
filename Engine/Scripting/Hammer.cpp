2968a1ea105ebab97f23a266a49e59b9c4cb3312
Ariadna Cortes <83670087+acpuig@users.noreply.github.com>
2024-06-24 19:28:27 +0200

    ApplySpecialEffects() hammer

--- Engine/Scripting/Hammer.cpp
+++ Engine/Scripting/Hammer.cpp
@@ -1,5 +1,7 @@
 #include "Hammer.h"
-#include "Application.h"
+#include "Enemy.h"
+#include "ScriptComponent.h"
+#include "GameObject.h"
 
 Hammer::Hammer(BoxColliderComponent* collider, TrailComponent* trail) : MeleeWeapon(collider, trail)
 {
@@ -18,5 +20,10 @@ void Hammer::PlayHitSound()
 
 void Hammer::ApplySpecialEffects(GameObject* enemy)
 {
+    Enemy* enemyScript = reinterpret_cast<Enemy*>(reinterpret_cast<ScriptComponent*>(enemy->GetComponent(ComponentType::SCRIPT))->GetScriptInstance());
+    if (enemyScript)
+    {
+        enemyScript->PushBack();
+    }
 }
 
