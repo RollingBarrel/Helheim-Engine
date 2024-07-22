fad50674db30fb21c5bec2d5a502e9a30c8fdc1d
Aileck <61742408+Aileck@users.noreply.github.com>
2024-07-22 23:13:25 +0800

    Feature/electrica trap/#86byqbmef (#357)
    * create new class
    
    * ddefine functions
    
    * trap atribute
    
    * electric trap feature
    
    * Update state machine metas
    
    * fix braket
    
    * make script configurable
    
    * add sfx
    
    * trap sfx prefab
    
    * update sfx
    
    * update scene with trap
    
    * improve trap
    
    * improve code
    
    * update some code
    
    * change some variable name
    
    * upload model
    
    * add model
    
    * update sfx / scene
    
    * upload emeta
    
    * new traps
    
    ---------
    
    Co-authored-by: carlosgspsp <46396509+carlosgspsp@users.noreply.github.com>

--- Engine/Scripting/Enemy.cpp
+++ Engine/Scripting/Enemy.cpp
@@ -36,10 +36,18 @@ void Enemy::Update()
 {
 	if (GameManager::GetInstance()->IsPaused()) return;
 
-	if (mDeath)
+	if (mIsParalyzed)
 	{
-		Death();
+		if (mParalyzedTimerScript.Delay(mParalyzedDuration))
+		{
+			Paralyzed(mParalysisSeverityLevel, false);
+		}
 	}
+
+    if (mDeath)
+    {
+        Death();
+    }
 
 	if (!mBeAttracted)
 	{
@@ -176,11 +184,6 @@ void Enemy::Death()
 	}
 }
 
-bool Enemy::IsChasing()
-{
-	return (mCurrentState == EnemyState::CHASE);
-}
-
 void Enemy::PushBack()
 {
 	float3 direction = mGameObject->GetWorldPosition() - mPlayer->GetWorldPosition();
@@ -203,6 +206,25 @@ void Enemy::Init()
 		mAiAgentComponent->StartCrowdNavigation();
 	}
 }
+
+void Enemy::Paralyzed(float percentage, bool paralyzed)
+{
+	if (paralyzed)
+	{
+		mIsParalyzed = true;
+		mSpeed *= percentage;
+		mParalyzedTimerScript = TimerScript();
+		mParalysisSeverityLevel = percentage;
+	}
+	else 
+	{
+		mIsParalyzed = false;
+		mSpeed /= percentage;
+
+		mParalysisSeverityLevel = 1.0f;
+	}
+}
+
 
 void Enemy::DropItem()
 {
