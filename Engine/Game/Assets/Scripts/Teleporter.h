d0e04364dcb1c8ee4563a143ce030458f467c9f6
OriolGallego2001 <oriol.gallego01@estudiant.upf.edu>
2024-07-22 12:12:47 +0200

    Test modify teleporter script

--- Engine/Game/Assets/Scripts/Teleporter.h
+++ Engine/Game/Assets/Scripts/Teleporter.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <Script.h>
 #include "Macros.h"
+#include "float3.h"
+
 
 class BoxColliderComponent;
 struct CollisionData;
@@ -16,15 +18,23 @@ public:
 	~Teleporter();
 
 	void Start() override;
+	void Update() override;
 
-	void Teleport();
 
+	
 
 	void OnCollisionEnter(CollisionData* collisionData);
 
 private:
 	
+	float3 LerpPosition();
 	GameObject* mDestination;
 	BoxColliderComponent* mCollider;
+	float mDuration = 4.0f; // Expose as script variable
+	float mCurrentTime = 0.0f;
+	bool mIsTriggered = false;
+	float mDistance = 0.0f;
+	float3 mDirection;
+	float3 mStartPos;
 };
 
--- Engine/Scripting/Teleporter.cpp
+++ Engine/Scripting/Teleporter.cpp
@@ -2,7 +2,7 @@
 
 #include "GameObject.h"
 #include <BoxColliderComponent.h>
-
+#include "GameApp.h"
 
 
 CREATE(Teleporter)
@@ -30,6 +30,24 @@ void Teleporter::Start()
     {
         mCollider->AddCollisionEventHandler(CollisionEventType::ON_COLLISION_ENTER, new std::function<void(CollisionData*)>(std::bind(&Teleporter::OnCollisionEnter, this, std::placeholders::_1)));
     }
+
+    mDistance = mGameObject->GetWorldPosition().Distance(mDestination->GetWorldPosition());
+    mDirection = mDestination->GetWorldPosition().Sub(mGameObject->GetWorldPosition()).Normalized();
+    mStartPos = mGameObject->GetWorldPosition();
+}
+
+void Teleporter::Update()
+{
+    if (mIsTriggered)
+    {
+        mCurrentTime += GameApp->GetDt();
+        mGameObject->SetWorldPosition(LerpPosition());
+        if (mCurrentTime > mDuration)
+        {
+            mIsTriggered = false;
+            mCurrentTime = 0.0f;
+        }
+    }
 }
 
 
@@ -37,6 +55,36 @@ void Teleporter::OnCollisionEnter(CollisionData* collisionData)
 {
     if (collisionData->collidedWith->GetTag() == "Player")
     {
+        // TODO: Pause player movement
         collisionData->collidedWith->SetWorldPosition(mDestination->GetWorldPosition());
+        mIsTriggered = true;
     }
+}
+
+float3 Teleporter::LerpPosition()
+{
+    if (mCurrentTime < 0.0f)
+    {
+        return mStartPos;
+    }
+    else if (mCurrentTime > mDuration)
+    {
+        return mDestination->GetWorldPosition();
+    }
+    float halfDuration = mDuration / 2;
+    float maxVelocity = mDistance / mDuration;
+
+    float lerpFactor = 0.0f;
+
+    if (mCurrentTime < halfDuration)
+    {
+        lerpFactor = 0.5f * (maxVelocity / halfDuration) * (mCurrentTime * mCurrentTime);
+    }
+    else
+    {
+        float distAcc = 0.5f * (maxVelocity / halfDuration) * (halfDuration * halfDuration);
+        lerpFactor = distAcc + maxVelocity * (mCurrentTime - halfDuration);
+    }
+
+    return mStartPos + mDirection * lerpFactor;
 }
