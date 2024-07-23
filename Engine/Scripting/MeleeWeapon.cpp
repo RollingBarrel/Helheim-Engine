ceac1a7d51e07041b78a41205c944e10c2f5a858
carlosgspsp <46396509+carlosgspsp@users.noreply.github.com>
2024-07-23 22:11:36 +0200

    crash fix

--- Engine/Scripting/MeleeWeapon.cpp
+++ Engine/Scripting/MeleeWeapon.cpp
@@ -24,7 +24,8 @@ MeleeWeapon::MeleeWeapon(BoxColliderComponent* collider, TrailComponent* trail) 
         );
     }
     mTrail = trail;
-    mTrail->SetEnable(false);
+    if (mTrail) mTrail->SetEnable(false);
+
     mPlayerGO = GameManager::GetInstance()->GetPlayer();
     mPlayerController = GameManager::GetInstance()->GetPlayerController();
     mLastComboStartTime = 0.0f;
