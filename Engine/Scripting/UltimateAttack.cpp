ef94b22d40af10bca61edd381342df61b084d825
Jaico1 <98921924+Jaico1@users.noreply.github.com>
2024-08-24 20:19:34 +0200

    Improve ultimate collision effect
    Using ray cast to change the distance of the laser

--- Engine/Scripting/UltimateAttack.cpp
+++ Engine/Scripting/UltimateAttack.cpp
@@ -2,6 +2,10 @@
 #include "Application.h"
 #include "GameObject.h"
 #include "Enemy.h"
+#include "MathFunc.h"
+
+#include "Physics.h"
+#include "Geometry/Ray.h"
 
 #include "ScriptComponent.h"
 #include "BoxColliderComponent.h"
@@ -50,4 +54,23 @@ void UltimateAttack::OnCollisionEnter(CollisionData* collisionData)
             }
         }
     }
+    else if (collisionGO->GetTag() != "Player" && collisionGO->GetTag() != "Drop" && collisionGO->GetTag() != "Trap")
+    {
+         //float distance = Distance(float2(collisionGO->GetWorldPosition().x, collisionGO->GetWorldPosition().z), float2(mGameObject->GetWorldPosition().x, mGameObject->GetWorldPosition().z)) / 10;
+         //Clamp01(distance);
+         float3 currentScale = mGameObject->GetLocalScale();
+         ////if (distance < currentScale.z)
+         //   mGameObject->SetLocalScale(float3(currentScale.x, currentScale.y, distance));
+        Hit hit;
+        Ray ray;
+        ray.dir = mGameObject->GetFront();
+        ray.pos = mGameObject->GetWorldPosition();
+
+        Physics::Raycast(hit, ray, 10.0f);  
+        if (hit.IsValid()) {
+            float distance = Distance(float2(hit.mHitPoint.x, hit.mHitPoint.z), float2(mGameObject->GetWorldPosition().x, mGameObject->GetWorldPosition().z)) / 10;
+            mGameObject->SetLocalScale(float3(currentScale.x, currentScale.y, distance));
+        }
+    }
+    else mGameObject->SetLocalScale(float3(1.0, 1.0, 1.0));
 }
