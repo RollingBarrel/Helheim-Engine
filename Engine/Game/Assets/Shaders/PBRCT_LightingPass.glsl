1c07759b04d587f4cc1fe32d1f35d1cf67c42b3b
bottzo <45202057+bottzo@users.noreply.github.com>
2024-09-14 19:46:48 +0200

    working no points

--- Engine/Game/Assets/Shaders/PBRCT_LightingPass.glsl
+++ Engine/Game/Assets/Shaders/PBRCT_LightingPass.glsl
@@ -67,8 +67,8 @@ layout(binding = 6)uniform samplerCube diffuseIBL;
 layout(binding = 7)uniform sampler2D environmentBRDF;
 uniform uint numLevels;
 
-layout(binding = 10) uniform usamplerBuffer pointLightList;
-layout(binding = 11) uniform usamplerBuffer spotLightList;
+layout(binding = 10) uniform isamplerBuffer pointLightList;
+layout(binding = 11) uniform isamplerBuffer spotLightList;
 layout(location = 2) uniform uint lightListSize;
 layout(location = 3) uniform uvec2 numTiles;
 layout(location = 4) uniform uvec2 tileSize;
@@ -146,8 +146,8 @@ void main()
 	const uvec2 currTile = uvec2(gl_FragCoord.xy) / tileSize;
 	const uint tileIdx = currTile.y * numTiles.x + currTile.x;
 	//Point lights
-	int idx = int(texelFetch(pointLightList, int(tileIdx * lightListSize)).x);
-	for (uint i = 0; i < lightListSize && idx != 255; idx = int((texelFetch(pointLightList, int(tileIdx * lightListSize + i))).x))
+	int idx = texelFetch(pointLightList, int(tileIdx * lightListSize)).x;
+	for (int i = 0; i < lightListSize && idx != -1; idx = texelFetch(pointLightList, int(tileIdx * lightListSize + i)).x)
 	{
 		PointLight pLight = pLights[idx];
 		vec3 mVector = pos - pLight.pos.xyz;
@@ -159,8 +159,8 @@ void main()
 	}
 	
 	//Spot lights
-	idx = int(texelFetch(spotLightList, int(tileIdx * lightListSize)).x);
-	for (int i = 0; i < lightListSize && idx != 255; idx = int((texelFetch(spotLightList, int(tileIdx * lightListSize + i))).x))
+	idx = texelFetch(spotLightList, int(tileIdx * lightListSize)).x;
+	for (int i = 0; i < lightListSize && idx != -1; idx = texelFetch(spotLightList, int(tileIdx * lightListSize + i)).x)
 	{
 		SpotLight sLight = sLights[idx];
 		//Shadows
