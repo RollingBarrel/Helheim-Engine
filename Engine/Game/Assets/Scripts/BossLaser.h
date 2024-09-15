887c5acdaee7a6fe7421f8a10903d9b064b83db1
Ariadna Cortes <83670087+acpuig@users.noreply.github.com>
2024-09-15 13:27:47 +0200

    Rewrite of the class + vfx

--- Engine/Game/Assets/Scripts/BossLaser.h
+++ Engine/Game/Assets/Scripts/BossLaser.h
@@ -28,27 +28,19 @@ public:
     void Init(float damage, float duration, float distance, float speed);
 
 private:
-
     void Charge();
     void Fire();
     void Cooldown();
-    void SpawnEyeBalls();
-    void SpawnEyeBall();
-
-    void ReturnEyeBallsToPool();
 
     LaserState mCurrentState = LaserState::IDLE;
 
     float mDamage = 0.0f;
     float mLaserDistance = 0.0f;
     float mLaserSpeed = 0.0f;
-    float mBossDistance = 1.5f;
     float mStateTime = 0.0f;
     float mChargeTime = 1.0f;
-    float mLaserEnemyDuration = 0.0f;
+    float mLaserDuration = 0.0f;
     float mCooldownDuration = 2.0f;
 
-    int mPoolSize = 6; 
-    std::vector<GameObject*> mActiveEyeBalls;      
-    std::vector<GameObject*> mInactiveEyeBall;   
+    GameObject* mLaserEyeBall = nullptr;  // Only one laser now
 };
