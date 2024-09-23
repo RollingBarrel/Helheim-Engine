4aa211a16bf850f6f86e3155b31e8e73cff96a95
carlosgspsp <46396509+carlosgspsp@users.noreply.github.com>
2024-09-23 19:53:30 +0200

    Different mouse icon support (#500)

--- Engine/Scripting/GameManager.cpp
+++ Engine/Scripting/GameManager.cpp
@@ -4,6 +4,7 @@
 #include "Application.h"
 #include "ModuleScene.h"
 #include "ModuleInput.h"
+#include "ModuleWindow.h"
 
 #include "GameObject.h"
 #include "ScriptComponent.h"
@@ -57,6 +58,8 @@ void GameManager::Awake()
 
 void GameManager::Start()
 {
+    //App->GetWindow()->SetCursor(857248271, 50, 50);
+
     if (mHudControllerGO)
     {
         ScriptComponent* script = static_cast<ScriptComponent*>(mHudControllerGO->GetComponent(ComponentType::SCRIPT));
