3f2d8a802b0935ed7debf1f2f5bd893d1219a397
bottzo <45202057+bottzo@users.noreply.github.com>
2024-09-24 12:23:54 +0200

    Feature/optimize ao (#502)
    * minimal test changes
    
    * optimized AO
    
    use the depth texture instead of the hole position texture
    Now it takes 1 ms to compute

--- Engine/Game/Assets/Shaders/ssao_fragment.glsl
+++ Engine/Game/Assets/Shaders/ssao_fragment.glsl
@@ -15,7 +15,7 @@ layout(std140, binding = 0) uniform CameraMatrices
 };
 
 
-layout(binding = 0) uniform sampler2D positions;
+layout(binding = 0) uniform sampler2D depth;
 layout(binding = 1) uniform sampler2D normals;
 
 
@@ -26,9 +26,22 @@ layout(location = 1) uniform float range;
 layout(location = 2) uniform float bias;
 
 
+float GetLinearZ(float inputDepth)
+{
+	return -proj[3][2] / (proj[2][2] + (inputDepth * 2.0 - 1.0));
+}
+
+vec3 GetViewPos(float depth, vec2 uvs)
+{
+	float viewZ = GetLinearZ(depth);
+	float viewX = (-viewZ * (uvs.x * 2.0 - 1.0)) / proj[0][0];
+	float viewY = (-viewZ * (uvs.y * 2.0 - 1.0)) / proj[1][1];
+	return vec3(viewX, viewY, viewZ);
+}
+
 mat3 CreateTangentSpace(const vec3 normal, const vec3 up)
 {
-	vec3 tangent = normalize(up-normal*dot(normal, up)); // Gram-Schmidt
+	vec3 tangent = normalize(up-(normal*dot(normal, up))); // Gram-Schmidt
 	vec3 bitangent = normalize(cross(tangent, normal));
 	return mat3(bitangent, tangent, normal);
 }
@@ -44,28 +57,28 @@ float GetSceneDepthAtSamplePos(in vec3 samplePos)
 {
 	vec4 clippingSpace = proj*vec4(samplePos, 1.0);
 	vec2 sampleUV = (clippingSpace.xy/clippingSpace.w)*0.5+0.5;
-	return (view * vec4(texture(positions, sampleUV).xyz, 1.0)).z;
+	return GetLinearZ(texture(depth, sampleUV).r);
 }
 
 void main()
 {
-//RECUERDA DE CAMBIAR EL OPENGL A SOLO UN COLOR
-	vec3 position = (view * vec4(texture(positions, uv).xyz,1.0)).xyz;
-	vec3 normal = mat3(view)*normalize(texture(normals, uv).xyz * 2.0 - 1.0);
+	vec3 viewPos = GetViewPos(texture(depth, uv).r, uv);
+	vec3 normal = mat3(view)*(texture(normals, uv).xyz * 2.0 - 1.0);
+	normal = normalize(normal);
 	mat3 tangentSpace = CreateTangentSpace(normal, GetRandomTangent());
-	float occlusion = 0;
+	float occlusion = 0.0f;
 
 	for(int i=0; i< KERNEL_SIZE; ++i)
 	{
-		vec3 samplePos = position+tangentSpace*kernelSamples[i];
+		vec3 samplePos = viewPos+tangentSpace*kernelSamples[i];
 		float samplePosDepth= GetSceneDepthAtSamplePos(samplePos);
-		if(samplePosDepth + bias > samplePos.z &&  abs(samplePosDepth-position.z) < range)
+		if((samplePosDepth + bias) > samplePos.z &&  abs(samplePosDepth-viewPos.z) < range)
 		{
-			float rangeCheck = smoothstep(0.0, 1.0, 1 / abs(position.z - samplePosDepth));
-			occlusion += (samplePosDepth >= samplePos.z + bias ? 1.0 : 0.0) * rangeCheck;
+			float rangeCheck = smoothstep(0.0, 1.0, 1 / abs(viewPos.z - samplePosDepth));
+			occlusion += ((samplePosDepth >= samplePos.z + bias) ? 1.0 : 0.0) * rangeCheck;
 		}			
 	}
 
 	//vec3 occlusionFactor = vec3(texture(ambientOcclusion,uv).r);
-	color = 1.0 - occlusion / float(KERNEL_SIZE);
+	color = 1.0f - occlusion / float(KERNEL_SIZE);
 }
