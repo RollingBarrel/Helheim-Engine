b9581fae734b5a6bbdf217827ca1d32a151feb1c
Ariadna Cortes <83670087+acpuig@users.noreply.github.com>
2024-10-04 18:05:58 +0200

    Update ElectricTrapController.h

--- Engine/Game/Assets/Scripts/ElectricTrapController.h
+++ Engine/Game/Assets/Scripts/ElectricTrapController.h
@@ -7,33 +7,31 @@
 struct CollisionData;
 class BoxColliderComponent;
 class GameObject;
-class ParticleSystemComponent;
 
-GENERATE_BODY(ElectricTrapController)
-
-class ElectricTrapController : public Script
+GENERATE_BODY(ElectricTrapController);
+class ElectricTrapController :
+	public Script
 {
-    FRIEND(ElectricTrapController)
-
+	FRIEND(ElectricTrapController)
 public:
-    ElectricTrapController(GameObject* owner);
-    ~ElectricTrapController();
+	ElectricTrapController(GameObject* owner);
+	~ElectricTrapController();
+	void Update() override;
+	void Start() override;
+	void OnCollisionEnter(CollisionData* collisionData);
 
-    void Update() override;
-    void Start() override;
-
-    void OnCollisionEnter(CollisionData* collisionData);
-
-	void SetAwake(bool awake) { mIsAwake = awake;  }
+	void SetAwake(bool awake) { mIsAwake = awake; }
 
 private:
 	bool IsInTrap(const GameObject* target);
 	void ActiveTrap(bool active);
 	void ActiveTrapFisrtTime();
 
-    void ActiveTrap(bool active);
+	BoxColliderComponent* mCollider = nullptr;
+	std::vector<GameObject*> mInTrap;
 
-    BoxColliderComponent* mCollider = nullptr;
+	float mArea = 1.0f;
+	GameObject* mSfx = nullptr;
 
 	// Activation
 	bool mIsAwake = false; // Awake when player is close
@@ -46,8 +44,9 @@ private:
 	float mActivationDuration = 2.0f;
 	TimerScript mActivationIntervalTimer;
 	TimerScript mActivationDurationTimer;
-	
+
 	// Damage
 	float mDamageAmount = 5.0f;
 	float mSpeedReduction = 0.5f; // Reduce 50%
 };
+
