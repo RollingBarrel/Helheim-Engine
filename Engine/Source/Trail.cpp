c551f080a9e98ee359a40fd056d4fd748e7522f8
Akiramei11 <80475373+Akiramei11@users.noreply.github.com>
2024-10-05 13:25:02 +0200

    Improves Trails directioning on the first two points (#545)
    * Set width for line component
    
    * Fix of trail starts

--- Engine/Source/Trail.cpp
+++ Engine/Source/Trail.cpp
@@ -201,8 +201,11 @@ void Trail::AddFirstTrailPoint(GameObject* mOwner)
     float2 topPointTexCoord = float2(mPoints.front().mDistanceUV, 1);
     float2 botPointTexCoord = float2(mPoints.front().mDistanceUV, 0);
 
-    mBuffer.push_back({ botPointPos, botPointTexCoord, distUV, direction });
-    mBuffer.push_back({ topPointPos, topPointTexCoord, distUV, direction });
+    if (mPoints.size() > 1)
+    {
+        mBuffer.push_back({ botPointPos, botPointTexCoord, distUV, direction });
+        mBuffer.push_back({ topPointPos, topPointTexCoord, distUV, direction });
+    }
 
     mLastPointPosition = position;
     mMinDist = distUV;
@@ -234,8 +237,11 @@ void Trail::AddFirstTrailPoint(float3 position)
     float2 topPointTexCoord = float2(mPoints.front().mDistanceUV, 1);
     float2 botPointTexCoord = float2(mPoints.front().mDistanceUV, 0);
 
-    mBuffer.push_back({ botPointPos, botPointTexCoord, distUV, direction });
-    mBuffer.push_back({ topPointPos, topPointTexCoord, distUV, direction });
+    if (mPoints.size() > 1)
+    {
+        mBuffer.push_back({ botPointPos, botPointTexCoord, distUV, direction });
+        mBuffer.push_back({ topPointPos, topPointTexCoord, distUV, direction });
+    }
 
     mLastPointPosition = position;
     mMinDist = distUV;
@@ -260,7 +266,11 @@ void Trail::UpdateTrailComponent(GameObject* owner)
     float3 position = owner->GetWorldPosition();
 
     const float dposition = position.DistanceSq(mLastPointPosition);
-    if (dposition >= mMinDistance && mPoints.size() < mMaxPoints)
+    if (dposition >= mMinDistance and mPoints.size() < mMaxPoints)
+    {
+        AddFirstTrailPoint(owner);
+    }
+    else if (mPoints.size() == 1)
     {
         AddFirstTrailPoint(owner);
     }
@@ -271,6 +281,7 @@ void Trail::UpdateTrailComponent(GameObject* owner)
         RemoveLastTrailPoint();
     }
 }
+
 void Trail::UpdateTrailParticle(float3 position)
 {
     const float dposition = position.DistanceSq(mLastPointPosition);
@@ -278,6 +289,11 @@ void Trail::UpdateTrailParticle(float3 position)
     {
         AddFirstTrailPoint(position);
     }
+    else if (mPoints.size() == 1)
+    {
+        AddFirstTrailPoint(position);
+    }
+
 
     mTrailTime += App->GetDt();
     if (mPoints.size() > 1 and mMaxLifeTime > 0 and (mTrailTime - mPoints.back().mCreationTime) >= mMaxLifeTime)
@@ -319,6 +335,7 @@ void Trail::UpdateLineComponent(GameObject* origin, GameObject* final)
         mMaxDist = 0.0f;
     }
 }
+
 void Trail::SetImage(unsigned int resourceId)
 {
     App->GetResource()->ReleaseResource(mImage->GetUID());
