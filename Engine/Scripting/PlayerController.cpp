c776bbc25a8dc6748f570598d39af35801da7326
Jaico1 <98921924+Jaico1@users.noreply.github.com>
2024-10-20 19:33:20 +0200

    New Player with new Ultimate (#618)
    * Initial rework
    
    * stats test level 1
    
    * no damage when charging
    
    * No damage behind walls
    
    * Linear movement can follow game object
    
    * charge animation speed
    
    * final ult prefab
    
    * player with ult prefab
    
    * Player prefab
    
    * New players on levels

--- Engine/Scripting/PlayerController.cpp
+++ Engine/Scripting/PlayerController.cpp
@@ -1062,7 +1062,8 @@ void PlayerController::EnableUltimate(bool enable)
     UltimateAttack* ultimateScript = (UltimateAttack*)((ScriptComponent*)mUltimateGO->GetComponent(ComponentType::SCRIPT))->GetScriptInstance();
     ultimateScript->ResetTimer();
     if (mUltimateGO)
-    {
+    {   
+        mInUlt = enable;
         if(!enable) GameManager::GetInstance()->GetAudio()->Pause(SFX::PLAYER_ULTIMATE,mUltSound,true);
         mUltimateGO->SetEnabled(enable);
     }
@@ -1072,6 +1073,7 @@ void PlayerController::EnableChargeUltimate(bool enable)
 {
     if (mUltimateChargeGO)
     {
+        mInUlt = enable;
         if(enable) mUltSound = GameManager::GetInstance()->GetAudio()->Play(SFX::PLAYER_ULTIMATE); 
         mUltimateChargeGO->SetEnabled(enable);
         mUltiOuterChargeGO->SetEnabled(enable);
@@ -1115,7 +1117,7 @@ void PlayerController::InterpolateLookAt(const float3& target, float speed)
 void PlayerController::TakeDamage(float damage)
 {
     //GameManager::GetInstance()->GetAudio()->PlayOneShot(SFX::PLAYER_HIT, GameManager::GetInstance()->GetPlayer()->GetWorldPosition());
-    if (IsPlayerDashing()|| mGodMode)
+    if (IsPlayerDashing()|| mGodMode || mInUlt)
     {
         return;
     }
